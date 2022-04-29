21501232fe0c5e10101565c4ef768cb00d81c4b1
alvaro-freire <alvaro.freirea@udc.es>
2022-04-29 22:09:06 +0200

    create frontend structure

---
+++ pages/index.js
@@ -0,0 +1,13 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Wordle League</title>
+        <meta name="description" content="Wordle League App" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+    </div>
+  )
+}
