52bd8833a103f88850bfb374f36e376a99cb1df6
Jorge Teixeira <jorge.teixeira@bizaway.com>
2022-04-30 12:31:10 +0200

    add scoreboard

---
+++ pages/api/leagues/[id]/scoreboard.js
@@ -0,0 +1,51 @@
+import checkAuth from "../../../../lib/checkauth"
+import { ObjectId } from 'mongodb'
+import loadCollection from "../../../../lib/loadcollection"
+
+export default async function handler(req, res) {
+  if (req.method !== 'GET') return res.status(400).json({ status: 'Bad request' }) 
+  const leaguesCollection = await loadCollection('leagues')
+  const resultsCollection = await loadCollection('results')
+  const username = checkAuth({ req, res })
+  if (!username) return res.status(401).json({ status: 'Not authorized' })
+  const { id } = req.query
+  let query
+  try {
+    query = { _id: ObjectId(id) }
+  } catch {
+    return res.status(400).json({ status: 'Invalid id' })
+  }   
+  const league = await leaguesCollection.findOne(query)
+  if (!league) return res.status(404).json({ status: 'Not found' })
+  query = {
+    username: {
+      $in: league.participants
+    },
+    typeUrl: league.typeUrl
+  }
+  const results = await resultsCollection.find(query).toArray()
+  const scoreboard = {}
+  for (const result of results) {
+    if (scoreboard[result.username]) {
+      scoreboard[result.username].results += 1
+      scoreboard[result.username].points += result.points
+    } else {
+      scoreboard[result.username] = {}
+      scoreboard[result.username].results = 1
+      scoreboard[result.username].points = result.points
+    }
+  }
+  const response = Object.keys(scoreboard).map(k => {
+    return {
+      username: k,
+      results: scoreboard[k].results,
+      points: scoreboard[k].points
+    }
+  })
+  res.status(200).json({
+    position: response.sort((a, b) => a - b).findIndex(r => r.username === username) + 1,
+    scoreboard: response.sort((a, b) => a - b).map((x, i) => {
+      return { ...x, position: i + 1 }
+    })
+  })
+}
--- pages/api/results/index.js
+++ pages/api/results/index.js
@@ -23,15 +23,21 @@ function getPoints(emojis) {
 
 export default async function handler(req, res) {
   const username = checkAuth({ req, res })
-  if (!username) return res.status(401).json({ status: 'Not authorized' })
-  const { result } = req.body
-  const lines = result.split('\n').filter(r => r !== "");
-  const head = lines[0]
-  const typeUrl = lines[lines.length - 1]
-  const emojis = lines.slice(1, -1)
-  const points = getPoints(emojis)
-  const preparedResult = { username, head, typeUrl, emojis, points }
   const resultsCollection = await loadCollection('results')
-  const response = await resultsCollection.insertOne(preparedResult)
-  res.status(200).json({ ...preparedResult, _id: response.insertedId })
+  if (!username) return res.status(401).json({ status: 'Not authorized' })
+  if (req.method === 'POST') {
+    const { result } = req.body
+    const lines = result.split('\n').filter(r => r !== "")
+    const head = lines[0]
+    const typeUrl = lines[lines.length - 1]
+    const emojis = lines.slice(1, -1)
+    const points = getPoints(emojis)
+    const preparedResult = { username, head, typeUrl, emojis, points }
+    const response = await resultsCollection.insertOne(preparedResult)
+    return res.status(200).json({ ...preparedResult, _id: response.insertedId })
+  }
+  if (req.method === 'GET') {
+    const results = await resultsCollection.find().toArray()
+    return res.status(200).json(results)
+  }
 }
