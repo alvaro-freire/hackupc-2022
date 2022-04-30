91bb48d171880fffdca82aea2d80d1fcbfb30d7d
alvaro-freire <alvaro.freirea@udc.es>
2022-04-30 18:48:12 +0200

    put maxlength upload

--- components/textarea.js
+++ components/textarea.js
@@ -1,9 +1,9 @@
 function TextArea({ label, type, name }) {
   return (
-    <div className={'mx-auto text-center w-[300px] p-4'}>
+    <div className={'mx-auto text-center w-[350px] p-4'}>
       <label className='text-lg flex flex-col'>
         <p className='text-sm mb-2'>{label}</p>
-        <textarea rows={10} className={'text-sm w-full resize-none border border-black px-2 mx-auto'} type={type} name={name} />
+        <textarea maxLength={130} rows={10} className={'text-sm w-full resize-none border border-black px-2 mx-auto'} type={type} name={name} />
       </label>
     </div>
   )
