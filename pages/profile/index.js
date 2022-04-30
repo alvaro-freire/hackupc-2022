28799bad685dc495a0db88ee9e2bfc0e0fb8b9b7
alvaro-freire <alvaro.freirea@udc.es>
2022-04-30 12:30:51 +0200

    add profile mock data

--- pages/profile/index.js
+++ pages/profile/index.js
@@ -6,9 +6,21 @@ function Profile() {
     <>
       <Seo title='Home' />
       <Navbar />
-      <main>
-        <h2>Profile</h2>
-      </main>
+        <div className='my-auto text-center mt-6 w-[300px] mx-auto'>
+          <p className='font-bold text-lg'>PERFIL</p>
+        </div>
+        <div className='my-auto text-center mt-6 w-[300px] mx-auto border'>
+          <p className='my-2'>Username: Alvaro</p>
+        </div>
+        <div className='my-auto text-center mt-2 w-[300px] mx-auto border'>
+          <p className='my-2'>Nº ligas: 3</p>
+        </div>
+        <div className='my-auto text-center mt-2 w-[300px] mx-auto border'>
+          <p className='my-2'>Entregas totales: 17</p>
+        </div>
+        <div className='my-auto text-center mt-2 w-[300px] mx-auto border'>
+          <p className='my-2'>Puntos totales: 12550</p>
+        </div>
     </>
   )
 }
